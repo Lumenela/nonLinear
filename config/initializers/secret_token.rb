efe023f438c633acb3d680ef5984a6914c6b517e
Dedunovich Svetlana <dedunovich.sveta@gmail.com>
2012-12-06 11:44:12 +0300

    basic structure, bootstrap, home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NonLinear::Application.config.secret_token = '20b31376cad7ef0e288c606067df2b6e0df547324db1833947fa2c641f47a00eaa7e414711bc97ef18b957296f581a25736d6da687fcbb8d3e28eda926e899b7'
