95880113d62613d5f38e2700f3a52eaadaa2d4a7
Dedunovich Svetlana <dedunovich.sveta@gmail.com>
2012-12-15 00:47:43 +0300

    basic solution

--- app/assets/javascripts/solver.js
+++ app/assets/javascripts/solver.js
@@ -1,6 +1,72 @@
 $(document).ready(function(){
-	
-	$('#solve_button').live('click',function(){
+	$('#interval_help').popover();
+
+	$('label.error').hide();
+
+	$('#solution_div').hide();
+
+	$('#collapseOne').collapse("hide");
+
+
+
+	$('#clear_button').click(function(){
+		$('#equation_field').val("");
+		$('#interval_input').val("");
+		$('#interval label.error').hide();
+		$('#solution').text("");
+		$('#solution_div').hide();
+	})
+
+	$('#interval_input').focusout(function(){
+		var re = new RegExp(/\[( *)?-?\d+(\.\d+)?( *)?,( *)?-?\d+(\.\d+)?( *)?\]+/);
+		console.log($(this).val());
+		if($(this).val().match(re))
+		{
+			console.log('success');
+			$('#interval label.error').hide();
+		}
+		else{
+			$('#interval label.error').show();
+		}
 
 	})
+
+	$('#solve_button').click(function(){
+		console.log($('#equation_field').val());
+		if($('#equation_field').val().length == 0 ){
+			$('#equation_field').focus();
+			return;
+		}
+		if($('#interval_input').val().length == 0){
+			$('#interval_input').focus();
+			return;
+		}
+		var str = {input: $('#equation_field').val() };
+		 $.ajax({
+			type:'POST',
+			dataType: 'json',
+			url: '/non_linear/solveEquation',
+			data: str,
+			success: function(data){
+            console.log(data);
+            $('#solution_div').show();
+            $('#solution').text(data);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown){
+            console.log(XMLHttpRequest.readyState);
+            console.log(XMLHttpRequest.status);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+		})
+	})
+
+	$('#second_variant').click(function(){
+			var first = $('#first_variant').text();
+			var second = $(this).text();
+			$(this).text(first);
+			$('#first_variant').text(second);
+			$('#first_variant').append('<span class="caret"></span>');
+		})
 })
+
--- app/controllers/non_linear_controller.rb
+++ app/controllers/non_linear_controller.rb
@@ -1,8 +1,17 @@
+require 'json'
 class NonLinearController < ApplicationController
   def solve
   end
 
   def solveEquation
-  	render :text =>'solution will be here'
+  	render :text =>eval(params[:input]).inspect
+  end
+
+  def parseInput(exp)
+  	return eval(expression).inspect
+	rescue => e
+		#return e.to_s + "\n" + e.backtrace.join("\n")
+		return "fail"
+  	
   end
 end
